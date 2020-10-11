9ded8b9b00b41ddae63e2e688603b3955895a86d
chenmingwei <chen_mingwei@126.com>
2020-10-11 08:08:26 +0000

    完成用户注册功能

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -9,12 +9,14 @@
     <link rel="stylesheet" href="{{ mix('css/app.css') }}">
 </head>
 <body>
-
     @include('layouts._header')
 
     <div class="container">
-        @yield('content')
-        @include('layouts._footer')
+        <div class="offset-md-1 col-md-10">
+            @include('shared._messages')
+            @yield('content')
+            @include('layouts._footer')
+        </div>
     </div>
 </body>
 </html>
